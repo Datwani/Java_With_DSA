0dd61b45389381e183383a23c769d607f2f4cb98
Yash_Datwani <126910357+Datwani@users.noreply.github.com>
2024-08-25 19:05:53 +0530

    Add files via upload

---
+++ JAVA_with_DSA/kunal/1-first-java/1-first-java/Q3.java
@@ -0,0 +1,23 @@
+//Write a program to input principal, time, and rate (P, T, R) from the user and find Simple Interest.
+
+import java.util.Scanner;
+
+public class Q3 {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        float Si,P,R,T;
+
+        System.out.print(" Enter principal:");
+        P=sc.nextInt();
+
+        System.out.print(" Enter Rate:");
+        R=sc.nextInt();
+
+        System.out.print(" Enter time:");
+        T=sc.nextInt();
+
+
+Si=P*R*T/100;
+System.out.println("Simple Interest:"+Si);
+    }
+}
